990f8639e8fac9efec7a2e0ca978df98f8d53f52
joycesaquino <joycesaquino@gmail.com>
2018-07-02 10:26:47 -0300

    estruturando o projeto

---
+++ src/pages/timesheets/timesheets.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-timesheets',
+  templateUrl: 'timesheets.html'
+})
+export class TimesheetsPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
