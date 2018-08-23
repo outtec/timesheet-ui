5def45a16a06efd10b74b9b2f25c931c5299ab7b
joycesaquino <joycesaquino@gmail.com>
2018-08-23 10:22:05 -0300

    Label data de entrada

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    baseUrl: "https://secure-headland-16849.herokuapp.com",
+    baseUrl: "http://localhost:8080",
     bucketBaseURL:"",
     
     //"http://localhost:8080" "https://secure-headland-16849.herokuapp.com",
